5313dbd8f20a049241fb85bf5f58c9d8399c00dc
Di Zeng <willzeng@umd.edu>
2016-10-27 10:37:31 -0700

    added calAMRs:

--- data_processor/monthly_transformer.py
+++ data_processor/monthly_transformer.py
@@ -50,7 +50,7 @@ def monthPlus(month, value):
 
 def findFirstDayOpenValue(month, data_base):
 	for data in data_base:
-		if data[0] == month[0] and data[1] == month[1]:
+		if isSameMonth(data, month):
 			value = data[3]
 			return value
 	return None
@@ -58,7 +58,7 @@ def findFirstDayOpenValue(month, data_base):
 def findLastDayClosedValue(month, data_base):
 	target_month = monthPlus(month, 1)
 	for id, data in enumerate(data_base):
-		if data[0] == target_month[0] and data[1] == target_month[1]:
+		if isSameMonth(data, target_month):
 			value = data_base[id][4]
 			return value
 
@@ -69,6 +69,59 @@ def findAllDatesIndexByMonth(month, dates):
 			find_dates.append(date)
 	return find_dates
 
+def isSameMonth(date1, date2):
+	for x in range(2):
+		if date1[x] != date2[x]:
+			return False
+	return True
+
+def isSameDate(date1, date2):
+	for x in range(3):
+		if date1[x] != date2[x]:
+			return False
+	return True
+
+def getAMRs(month_date, monthly_database):
+
+	for id, data in enumerate(monthly_database):
+		if isSameMonth(month_date, data[0]):
+			current_month_id = id
+			break
+
+	start_month_id = id - 12
+	if start_month_id < 0:
+		return None
+
+	start_price = monthly_database[eval_month_id][1]
+	amr = []
+	for x in range(12):
+		eval_month_id = start_month_id + x
+		eval_price = monthly_database[eval_month_id][1]
+		amr.append(start_price, eval_price)
+	return amr
+
+def getADRs(date, daily_database):
+	for id, daily_database in enumerate(daily_database):
+		if isSameDate(date, data[0]):
+			current_date_id = id
+			break
+
+	start_date_id = current_date_id - 20
+	if start_date_id < 0
+		return None
+
+	start_price == daily_database
+
+def calAR(start_price, close_price):
+	return (close_price - start_price)/start_price
+
+def getInputData(monthly_database, daily_database):
+	input_data = []
+	for month_data in monthly_database:
+		amr = getAMRs(month_data[0],monthly_database)
+
+	# pass
+
 def main(DATA_ADDRESS):
 	data_address = os.path.abspath(DATA_ADDRESS)
 	data_files = os.listdir(data_address)
@@ -77,7 +130,7 @@ def main(DATA_ADDRESS):
 		data_input = []
 		stock_id = file
 		dates =[]
-		daily_data_base = []
+		daily_database = []
 		monthly_database = []
 		with open(file_path, 'rt') as csvfile:
 			reader = csv.DictReader(csvfile, delimiter=',', skipinitialspace=True)
@@ -89,7 +142,7 @@ def main(DATA_ADDRESS):
 				vclose = float(row['Close'])
 				vopen = float(row['Open'])
 				daily_return = (vclose - vopen)/vopen
-				daily_data_base.append( [date, vopen, vclose] )
+				daily_database.append( [date, vopen, vclose] )
 				if last_month is None:
 					last_month = date
 					last_month_value = vopen
@@ -100,14 +153,17 @@ def main(DATA_ADDRESS):
 						last_month_value = vopen
 						monthly_database.append( [date, vopen, monthly_return] )
 						print(monthly_database[-1])
+			getInputData(monthly_database, daily_database)
 
-			start_value = monthly_database[0][1]
-			for data in monthly_database:
-				amr.append((data[1] - start_value)/start_value)
-			plt.plot(amr)
-			plt.show()
+			# start_value = monthly_database[0][1]
+			# for data in monthly_database:
+			# 	amr.append((data[1] - start_value)/start_value)
+			# plt.plot(amr)
+			# plt.show()
 
 if __name__ == '__main__':
-	DATA_ADDRESS = "../data/"
-	main(DATA_ADDRESS)
+	for x in range(2):
+		print(x)
+	# DATA_ADDRESS = "../data/"
+	# main(DATA_ADDRESS)
 	# testParseDate()
