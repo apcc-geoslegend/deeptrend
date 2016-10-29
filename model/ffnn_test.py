55cd262a630d388a77957170ea5d814634d75807
Di Zeng <willzeng@umd.edu>
2016-10-28 18:09:28 -0700

    delted useless stuff

--- model/ffnn_test.py
+++ model/ffnn_test.py
@@ -1,30 +1,7 @@
-# Copyright 2015-2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""A very simple MNIST classifier.
-
-See extensive documentation at
-http://tensorflow.org/tutorials/mnist/beginners/index.md
-"""
-from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import argparse
-
-# Import data
 import sys
 import os
 sys.path.insert(0, os.path.abspath(".."))
@@ -80,8 +57,4 @@ def main(_):
   print(output)
 
 if __name__ == '__main__':
-  # parser = argparse.ArgumentParser()
-  # parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-  #                     help='Directory for storing input data')
-  # FLAGS = parser.parse_args()
   tf.app.run()
